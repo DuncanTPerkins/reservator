89270c19843c704927e3c990625ec1613100c5d6
weweboom <weweboom@gmail.com>
2014-10-22 22:28:58 -0400

    Initial creation of week class
    will make moving the description strings  between staff and students
    easier and less procedural.

---
+++ php/week.php
@@ -0,0 +1,17 @@
+<?php
+class week {
+  private $today = getdate();
+  private $mon = "";
+  private $tue = "";
+  private $wed = "";
+  private $thu = "";
+  private $fri = "";
+  private $mond = "";
+  private $tued = "";
+  private $wedd = "";
+  private $thud = "";
+  private $frid = "";
+
+
+}
+?>
