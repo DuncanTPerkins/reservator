242ee7069b1cb246fc6f881d49d7d581f6be63da
weweboom <weweboom@gmail.com>
2014-10-22 20:48:24 -0400

    goodbye index.html, we'll all miss you

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php if(session_is_registered(username)){ header( "location:index.html");} ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- google drive-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>UTK Delta Zeta Meal Reservations</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/login.css" />
+    <link href='http://fonts.googleapis.com/css?family=Chelsea+Market' rel='stylesheet' type='text/css'>
+
+    <!--[if IE]>
+        <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+        <body>
+            <div class="container">
+                <div class="row vertical-offset-50">
+                    <div class="col-md-10 col-md-offset-1">
+                        <div class="panel panel-default" id="main-login-panel">
+                            <div class="panel-heading" id="welcome">
+                                UTK Delta Zeta Meal Reservation Site
+                            </div>
+                            <div class="panel-body main-panel">
+
+                                <div class="col-md-6">
+
+                                    <div class="panel panel-default custom-panel">
+                                        <div class="panel-heading">
+                                            <div class="row-fluid user-row">
+                                                <img src="/images/deltazetalogowhitebg.jpg" class="img-responsive"
+                                                     id="deltazetalogo" alt="UTK Reservations" />
+                                            </div>
+                                        </div>
+                                        <div class="panel-body">
+                                            <form name="login" method="post" action="php/login.php" accept-charset="UTF-8" role="form" class="form-signin">
+                                                <fieldset>
+                                                    <label class="panel-login">
+                                                        <div class="login_result"></div>
+                                                    </label>
+
+                                                        <input name="username" class="form-control" placeholder="E-mail" id="username"
+                                                           type="text">
+
+                                                    <input class="form-control" name="password" placeholder="Password" id="password"
+                                                           type="password">
+                                                    <div class="checkbox">
+                                                        <label>
+                                                            <input type="checkbox" id="remember">Remember Me
+                                                        </label>
+                                                    </div>
+                                                    <div class="col-md-6 col-s-6 button-container">
+                                                    <input class="btn btn-lg btn-success btn-block" name="login" type="submit"
+                                                           id="login" value="Log in »">
+                                                    </div>
+                                                    <div class="col-md-6 col-s-6 button-container">
+                                                    <input class="btn btn-lg btn-success btn-block" type="submit"
+                                                           id="register" value="Register »">
+                                                    </div>
+                                                </fieldset>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="panel panel-default">
+                                        <div class="panel-body custom-panel">
+                                            <img src="/images/girls.png" class="img-responsive img-rounded">
+                                            <div class="panel-body">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Welcome to the
+                                                meal reservation site for the UTK chapter of Delta Zeta. Login to
+                                                the form to begin making reservations!
+                                                <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Questions/Comments? E-mail Liz
+                                                Buckner at lizbuckner@comcast.net</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
