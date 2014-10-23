e336f4d13f93158bfd6b4220dc63361a2d675ba7
tjbuckner <teejay.buckner@gmail.com>
2014-10-22 22:41:33 -0400

    added modal

--- php/login.php
+++ php/login.php
@@ -41,3 +41,18 @@ echo "Wrong Username or Password";
 ob_end_flush();
 
 ?>
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="myModalLabel">Confirm Reservation</h4>
+            </div>
+            <div class="modal-body">
+                You have been logged out.
+            </div>
+
+        </div>
+    </div>
+</div>
