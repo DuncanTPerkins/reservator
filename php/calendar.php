482fb7a0f15b36e845601859464bf4702c0f7abc
weweboom <weweboom@gmail.com>
2014-10-23 20:29:57 -0400

    THESE COMMIT DESCRIPTIONS HELP NO ONE

--- php/calendar.php
+++ php/calendar.php
@@ -79,7 +79,7 @@ $startingDay = new DateTime($year . "-" . $month . "-" . $day);
 $loopDay = $startingDay;
 
 for($j=0; $j<5; $j++) {
-    echo "<th>" . $loopDay->format('tndrh\t') . "</th>";
+    echo "<th>" . $loopDay->format('tndr') . "</th>";
     $loopDay->modify('+1 day');
 }
 
