689c745921ea550d8a0c5e1f49a376249da24f4c
weweboom <weweboom@gmail.com>
2014-10-23 20:30:38 -0400

    :(

--- php/calendar.php
+++ php/calendar.php
@@ -79,7 +79,7 @@ $startingDay = new DateTime($year . "-" . $month . "-" . $day);
 $loopDay = $startingDay;
 
 for($j=0; $j<5; $j++) {
-    echo "<th>" . $loopDay->format('tndr') . "</th>";
+    echo "<th>" . $loopDay->format('ndr') . "</th>";
     $loopDay->modify('+1 day');
 }
 
