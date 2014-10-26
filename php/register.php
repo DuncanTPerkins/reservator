4ce1714b53b827e3c03c12bd4aca4274559595ea
weweboom <weweboom@gmail.com>
2014-10-26 11:39:59 -0400

    testing

--- php/register.php
+++ php/register.php
@@ -16,7 +16,6 @@ $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $sql1 = mysqli_query($conn, "SELECT * FROM STUDENTS WHERE '$email' = email");
 $rows = mysqli_num_rows($sql1);
-print_r($rows);
 if($rows!=0) {
     session_start();
     $_SESSION['nouse'] = 1;
