eb0e7062c4c2edde7249344b6a2afa4547d0ceb7
weweboom <weweboom@gmail.com>
2014-10-26 11:54:25 -0400

    testing

--- php/register.php
+++ php/register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $DBServer="localhost" ; $DBUser="tjdpproj_user" ; $DBPass="Bookerer1" ; $DBName="tjdpproj_db" ;
 
 //connect
@@ -17,7 +18,6 @@ $lname = $_POST['lname'];
 $sql1 = mysqli_query($conn, "SELECT * FROM STUDENTS WHERE '$email' = email");
 $rows = mysqli_num_rows($sql1);
 if($rows!=0) {
-    session_start();
     $_SESSION['nouse'] = 1;
     header("location:../register.php");
 }
@@ -27,7 +27,6 @@ $sql2 = "INSERT INTO STUDENTS(pk_student_id, username, password, email, phone, f
 VALUES (NULL, '$email', '$password', '$email', '$phone', '$fname', '$lname')";
 
 if (mysqli_query($conn, $sql2)) {
-    session_start();
     $_SESSION['username'] = $email;
     $_SESSION['name'] = $fname . " " . $lname;
     header("location:../calendar.php");
