33c3a15d48915174034b883abb4a7a1d60265e7d
weweboom <weweboom@gmail.com>
2014-10-26 06:16:29 -0400

    Making register.html do things

---
+++ php/register.php
@@ -0,0 +1,30 @@
+<?php
+$DBServer="localhost" ; $DBUser="tjdpproj_user" ; $DBPass="Bookerer1" ; $DBName="tjdpproj_db" ;
+
+//connect
+$conn= new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Database connection failed: " . mysqli_connect_error();
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$fname = $_POST['fname'];
+$phone = $_POST['lname'];
+
+
+$sql = "INSERT INTO STUDENTS (username, password, email, phone, fname, lname)
+VALUES ('unused', $password, $email, $phone, $fname, $lname)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
