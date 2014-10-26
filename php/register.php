17aab8b1065119920de869b9f65830e130904783
weweboom <weweboom@gmail.com>
2014-10-26 11:39:24 -0400

    testing

--- php/register.php
+++ php/register.php
@@ -16,12 +16,14 @@ $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $sql1 = mysqli_query($conn, "SELECT * FROM STUDENTS WHERE '$email' = email");
 $rows = mysqli_num_rows($sql1);
-print_r($sql1);
+print_r($rows);
 if($rows!=0) {
     session_start();
     $_SESSION['nouse'] = 1;
     header("location:../register.php");
 }
+
+else {
 $sql2 = "INSERT INTO STUDENTS(pk_student_id, username, password, email, phone, fname, lname)
 VALUES (NULL, '$email', '$password', '$email', '$phone', '$fname', '$lname')";
 
@@ -32,6 +34,7 @@ if (mysqli_query($conn, $sql2)) {
     header("location:../calendar.php");
 } else {
     echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
+    }
 }
 
 mysqli_close($conn);
