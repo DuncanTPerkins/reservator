23583bcef931e3b75260fd8e8c110857cb6496ba
weweboom <weweboom@gmail.com>
2014-10-26 11:46:43 -0400

    haha, nope

--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <!-- google drive-->
@@ -22,7 +23,6 @@
     <script type="text/javascript">
     </script>
 <?php
-session_start();
 if($_SESSION['nouse'] == 1) {
 ?>
     <script>
