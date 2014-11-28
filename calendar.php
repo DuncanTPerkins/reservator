3da99de21c4a1e1a833432fe2ad05704d43faf8a
weweboom <weweboom@gmail.com>
2014-11-28 11:16:08 -0500

    testing

--- calendar.php
+++ calendar.php
@@ -278,10 +278,16 @@ $day++;
                                                   <div class="col-md-12 lunch">
                                                        <span class="label label-primary">LUNCH:</span>
                                                        <br>
-                                                       <div class="<?php echo $class[0] . '"' . " id='$mealid[0]'>"?>
+                                                       <div class="<?php echo $class[0] . '"' . " id='$mealid[0]'"?>>
                                                             <?php echo $meals[0]; ?>
                                                             <?php echo $checks[0]; ?>
                                                             </div>
+
+                                                 </div>
+                                                 </div>
+                                            </div>
+                                                 </div>
+                                                 </div>
 
                                                        </div>
                                                   </div>
