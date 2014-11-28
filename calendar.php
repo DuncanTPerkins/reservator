f9c9e374e048a7e9c6b1b2f23b34e56bce421b3d
weweboom <weweboom@gmail.com>
2014-11-28 09:43:41 -0500

    testing

--- calendar.php
+++ calendar.php
@@ -42,6 +42,7 @@ while($day < $day1+5) {
 $dayfield = $year . "-" . $month . "-" . $day;
 $result = mysqli_query($conn, "SELECT * FROM MEAL WHERE date = '" . $dayfield ."' ORDER BY meal_type");
 $result2 = mysqli_query($conn, "SELECT * FROM RESERVATION WHERE student = '$studentid' and date = '$dayfield'");
+print_r($result2);
 $count=mysqli_num_rows($result);
 $count2 = mysqli_num_rows($result2);
     while($row2 = mysqli_fetch_array($result2, MYSQL_ASSOC)) {
