dc66cb4a3a7dfb311cb174419cff89b6589add10
weweboom <weweboom@gmail.com>
2014-11-27 23:22:09 -0500

    test db insertion

--- php/reserve.php
+++ php/reserve.php
@@ -1,3 +1,21 @@
 <?php
-print_r($_SESSION);
+$DBServer="localhost"; $DBUser="tjdpproj_user"; $DBPass="Bookerer1"; $DBName="tjdpproj_db";
+//connect
+$conn= new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Database connection failed: " . mysqli_connect_error();
+}
+
+$studentid = $_POST['studentid'];
+$mealid = $_POST['mealid'];
+$datetime = $_POST['datetime'];
+
+if(mysqli_query($conn, "INSERT INTO RESERVATION(student_id, meal, date) VALUES('$studentid', '$mealid', '$datetime')")) {
+
+ echo "success";
+}
+mysqli_close($conn);
+
 ?>
