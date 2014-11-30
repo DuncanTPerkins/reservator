0aae666414024cd432c4ac0f26ddf25159778ce4
weweboom <weweboom@gmail.com>
2014-11-30 04:55:58 -0500

    testing

--- staff.php
+++ staff.php
@@ -302,7 +302,7 @@
                 var badindex = description.indexOf(badstring);
                 var newstring = description.substr(0, badindex-1);
                 newstring = newstring.trim();
-                $("#fieldentry").val(newstring);
+                $("#fieldentry").text(newstring);
                 //take the value from the meal description for this meal, and put it into the modal
             });
 
@@ -881,7 +881,7 @@
                 <form action="php/insert.php" method="POST">
             <input value="<?php $today = getdate(); $year = $today[year]; $month = $today[mon]; $day = $today[mday]; echo $year . "-" . $month . "-" . $day; ?>" name="datetime" style="display: none;">
             <input value="0" id="mealid" name="mealid" style="display: none;">
-                    <textarea id="fieldentry" rows="4" value="" cols="50"></textarea>
+                    <textarea id="fieldentry" rows="4" cols="50"></textarea>
         <button type="submit" class="btn btn-primary formsubmit">Ok</button>
           </form>
 
