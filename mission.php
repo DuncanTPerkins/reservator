404cd2421d585d1af6beaca207be5d9ccfcac774
weweboom <weweboom@gmail.com>
2014-12-02 09:55:59 -0500

    testing

--- mission.php
+++ mission.php
@@ -30,8 +30,9 @@
                     <a class="navbar-brand" id="navbar-brand" href="php/gohome.php">
                         <div class="hidden-sm hidden-xs" id="navbar-brand-padding">&nbsp;</div>UTK Delta Zeta Meal Reservation</a>
                 </div>
-                <li class="active"><a href="#">Mission Statement</a></li>
-
+                <ul class="nav navbar-nav">
+                                    <li class="active"><a href="#">Mission Statement</a></li>
+                </ul>
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
