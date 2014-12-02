9626f6782c700a7389bfe89e780589f1e511f502
weweboom <weweboom@gmail.com>
2014-12-02 08:58:15 -0500

    testing

--- staff.php
+++ staff.php
@@ -831,54 +831,6 @@
 
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
-                    </button>
-                    <h4 class="modal-title" id="myModalLabel">List of Reservations for Wednesday, September 3rd</h4>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-bordered">
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>E-Mail</th>
-                            <th>Phone Number</th>
-                        </tr>
-                        <tr>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                        </tr>
-                        <tr>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                        </tr>
-                        <tr>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                        </tr>
-                        <tr>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                            <td>Placeholder</td>
-                        </tr>
-                    </table>
-
-                </div>
-            </div>
-        </div>
-    </div>
-
 
         <div class="modal fade" id="changeDesc" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -893,7 +845,7 @@
             <input value="0" id="mealid" name="mealid" style="display: none;">
             <input value="" id="datetime" name="datetime" style="display: none;">
             <input value="2" id="meal_type" name="meal_type" style="display: none;">
-            <input id="description" name="description" value="merp">
+            <input id="description" name="description">
         <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
         <button type="submit" class="btn btn-primary formsubmit">Ok</button>
           </form>
