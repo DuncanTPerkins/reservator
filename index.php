865036b540d19f61af8d85038c732f7e7a33e6c6
weweboom <weweboom@gmail.com>
2014-12-02 10:34:59 -0500

    testing

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ if(session_is_registered(username)){ header( "location:calendar.php");}?>
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Invalid!</h4>
+        <h4 class="modal-title">Invalid!</h4>
       </div>
       <div class="modal-body">
         Invalid Username or Password. Please try again.
